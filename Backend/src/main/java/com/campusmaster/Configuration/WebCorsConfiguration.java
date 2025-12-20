df06f2ffccbc5c88d47e5bd0c57ba0c670d4be93
Amadou Boubacar Niang <amadouniang2001@gmail.com>
2025-12-20 17:53:17 +0000

    changes

--- Backend/src/main/java/com/campusmaster/Configuration/WebCorsConfiguration.java
+++ Backend/src/main/java/com/campusmaster/Configuration/WebCorsConfiguration.java
@@ -15,7 +15,7 @@ public class WebCorsConfiguration {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://campus-master.vercel.app"));
         corsConfiguration.setAllowedHeaders(Arrays.asList(
             "Origin", "Access-Control-Allow-Origin", "Content-Type",
             "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
